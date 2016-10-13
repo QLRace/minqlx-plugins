a5614ddfff7df21e8995892017efd7418c422753
Christopher Stewart <cstewart90@users.noreply.github.com>
2016-10-13 19:16:52 +0100

    Added heaven_or_hell to GAUNTLET_AND_MG.

--- race.py
+++ race.py
@@ -34,7 +34,7 @@ GAUNTLET_AND_MG = ("blockworld", "caep4", "climbworld", "df_etleague", "df_extre
                    "vanilla_04", "vanilla_07", "vanilla_10", "walkathon", "weirdwild", "wraiths", "yellowtorture",
                    "run139", "inder_inder", "quartz", "timelock3", "daytime", "blub", "aa_lum", "kairos_nosf",
                    "aa_torture", "cube_torture", "track_comp", "track_comp_barriers", "dfru_xlarve", "track", "gl",
-                   "qportal")
+                   "qportal", "heaven_or_hell")
 
 PLASMA = ("think1", "xproject", "plasmax", "wub_junk", "pgultimate", "tinyplams", "df_lickcells", "df_lickcells2")
 ROCKET = ("runstolfer", "charon", "charon_bw", "kozmini1", "kozmini2", "kozmini3", "kozmini4", "kozmini5", "kozmini6",
