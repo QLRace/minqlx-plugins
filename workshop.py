0b3b2f179eaccbbf5b6ccfc18273a94fc1a96923
Christopher Stewart <cstewart90@users.noreply.github.com>
2017-02-17 00:11:41 +0000

    merged workshop.py

--- workshop.py
+++ workshop.py
@@ -37,9 +37,7 @@ MAP_IDS = {
     "pea_xweirdx": 850088922, "sdc_uk_01": 850731967, "pornstar_quickie3": 852060803, "timewaste": 852081302,
     "bumpyland": 852175366, "dfwc2014_2": 852839568, "pornvannestrafe1": 852978647, "dkr14": 853576545,
     "apo2": 853576826, "mlctf1beta": 728029244, "hexq3ctf1": 610686214, "ctf_orange": 728522755, "asteroid": 610695633,
-    "airmaxjumps2": 865244047
-    "apo2": 853576826, "mlctf1beta": 728029244, "hexq3ctf1": 610686214, "ctf_orange": 728522755, "asteroid": 610695633,
-    "pornstarghost2": 858065015, "airmaxjumps2": 865244047"
+    "pornstarghost2": 858065015, "airmaxjumps2": 865244047
 }
 
 
